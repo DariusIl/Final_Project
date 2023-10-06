c9faa2363c960de73b18d545cb67cb4357a0ed30
DariusIl <144364805+DariusIl@users.noreply.github.com>
2023-10-06 19:59:26 +0300

    surasiau importus

---
+++ main.py
@@ -0,0 +1,10 @@
+# import
+import pandas as pd
+import pandas as pd
+import seaborn as sns
+from bs4 import BeautifulSoup
+import requests
+import psycopg2
+import numpy as np
+import matplotlib.pyplot as plt
+
