3910ac0dc2388914a28890dc647fb327a5d8db2b
DariusIl <144364805+DariusIl@users.noreply.github.com>
2023-10-16 18:55:18 +0300

    papildziau

--- main.py
+++ main.py
@@ -3,79 +3,79 @@ import pandas as pd
 from selenium import webdriver
 import time
 
-# #  URL of the webpage for data scrap
-# target = "https://www.euroleaguebasketball.net/euroleague/stats/expanded/?size=1000&viewType=traditional&seasonMode=All&statisticMode=perGame"
+#  URL of the webpage for data scrap
+target = "https://www.euroleaguebasketball.net/euroleague/stats/expanded/?size=1000&viewType=traditional&seasonMode=All&statisticMode=perGame"
+
+# Initialize a Selenium WebDriver (ChromeDriver)
+driver = webdriver.Chrome()
+
+# Sending an HTTP GET request to the URL
+driver.get(target)
+
+# Waiting time for the page to load
+time.sleep(3)
+
+# Scroll down to load all rows, we set 30 to get 587 rows
+for _ in range(30):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+
+# Getting the page source after loading all rows
+page_source = driver.page_source
+
+# Close the Selenium WebDriver
+driver.quit()
+
+# Parse the HTML content and scrap data using BeautifulSoup
+soup = BeautifulSoup(page_source, 'html.parser')
+
+data = []
+# finding and extracting elements
+table = soup.find('div',
+                  class_='complex-stat-table_wrap__t4tzy custom-scrollbar_wrap__qkItA custom-scrollbar__hor__yG43Q')
 #
-# # Initialize a Selenium WebDriver (ChromeDriver)
-# driver = webdriver.Chrome()
-#
-# # Sending an HTTP GET request to the URL
-# driver.get(target)
-#
-# # Waiting time for the page to load
-# time.sleep(3)
-#
-# # Scroll down to load all rows, we set 30 to get 587 rows
-# for _ in range(30):
-#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#     time.sleep(2)
-#
-# # Getting the page source after loading all rows
-# page_source = driver.page_source
-#
-# # Close the Selenium WebDriver
-# driver.quit()
-#
-# # Parse the HTML content and scrap data using BeautifulSoup
-# soup = BeautifulSoup(page_source, 'html.parser')
-#
-# data = []
-# # finding and extracting elements
-# table = soup.find('div',
-#                   class_='complex-stat-table_wrap__t4tzy custom-scrollbar_wrap__qkItA custom-scrollbar__hor__yG43Q')
-# #
-# if table:
-#     rows = table.find_all('div', class_='complex-stat-table_row__Jiu1w')
-# # extracting data , skipping the header row (index 0)
-#     for row in rows[1:]:
-#         columns = row.find_all('div')
-# # replace and split data
-#         data.append({
-#             'Player': columns[1].text.strip(),
-#             'Team': columns[2].text.strip(),
-#             'GP': columns[3].text.strip(),
-#             'GS': columns[4].text.strip(),
-#             'Min': columns[5].text.strip().split(':')[0],
-#             'PTS': columns[6].text.strip(),
-#             'PM_2': columns[7].text.strip(),
-#             'PA_2': columns[8].text.strip(),
-#             'P2proc': columns[9].text.strip('%'),
-#             'PM_3': columns[10].text.strip(),
-#             'PA_3': columns[11].text.strip(),
-#             'P3proc': columns[12].text.strip('%'),
-#             'FTM': columns[13].text.strip(),
-#             'FTA': columns[14].text.strip(),
-#             'FTAproc': columns[15].text.strip('%'),
-#             'OR': columns[16].text.strip(),
-#             'DR': columns[17].text.strip(),
-#             'TR': columns[18].text.strip(),
-#             'AST': columns[19].text.strip(),
-#             'STL': columns[20].text.strip(),
-#             'TO': columns[21].text.strip(),
-#             'BLK': columns[22].text.strip(),
-#             'BLKA': columns[23].text.strip(),
-#             'FC': columns[24].text.strip(),
-#             'FD': columns[25].text.strip(),
-#             'PIR': columns[26].text.strip()
-#         })
-#
-# # # Creating DataFrame from the extracted data
-# df = pd.DataFrame(data)
-#
-# # Printing all rows of the DataFrame
-# print(df)
-# # save to CSV
-# df.to_csv('BasketAll4.csv')
+if table:
+    rows = table.find_all('div', class_='complex-stat-table_row__Jiu1w')
+# extracting data , skipping the header row (index 0)
+    for row in rows[1:]:
+        columns = row.find_all('div')
+# replace and split data
+        data.append({
+            'Player': columns[1].text.strip(),
+            'Team': columns[2].text.strip(),
+            'GP': columns[3].text.strip(),
+            'GS': columns[4].text.strip(),
+            'Min': columns[5].text.strip().split(':')[0],
+            'PTS': columns[6].text.strip(),
+            'PM_2': columns[7].text.strip(),
+            'PA_2': columns[8].text.strip(),
+            'P2proc': columns[9].text.strip('%'),
+            'PM_3': columns[10].text.strip(),
+            'PA_3': columns[11].text.strip(),
+            'P3proc': columns[12].text.strip('%'),
+            'FTM': columns[13].text.strip(),
+            'FTA': columns[14].text.strip(),
+            'FTAproc': columns[15].text.strip('%'),
+            'OR': columns[16].text.strip(),
+            'DR': columns[17].text.strip(),
+            'TR': columns[18].text.strip(),
+            'AST': columns[19].text.strip(),
+            'STL': columns[20].text.strip(),
+            'TO': columns[21].text.strip(),
+            'BLK': columns[22].text.strip(),
+            'BLKA': columns[23].text.strip(),
+            'FC': columns[24].text.strip(),
+            'FD': columns[25].text.strip(),
+            'PIR': columns[26].text.strip()
+        })
+
+# # Creating DataFrame from the extracted data
+df = pd.DataFrame(data)
+
+# Printing all rows of the DataFrame
+print(df)
+# save to CSV
+df.to_csv('BasketAll4.csv')
 
 # add import for pie chart
 import numpy as np
